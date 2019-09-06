f34ba2c15b20f0604ab69a13175a5f3f88b67b29
ammrats <amepop.1280@gmail.com>
2019-09-06 23:30:45 +0900

    pewered by takusann

--- AtCoder_Beginners_Selection/10.Daydream.php
+++ AtCoder_Beginners_Selection/10.Daydream.php
@@ -1,37 +1,3 @@
 <?php
-
-function checkString($str)
-{
-    $dream = 'dream';
-    $dreamer = 'dreamer';
-    $erase = 'erase';
-    $eraser = 'eraser';
-
-    if (
-        0 === strpos($str, $dreamer) &&
-        0 === preg_match('/^dreamerase/', $str)
-    ) {
-        $str = preg_replace('/^dreamer/', '', $str);
-    } elseif (0 === strpos($str, $dream)) {
-        $str = preg_replace('/^dream/', '', $str);
-    } elseif (0 === strpos($str, $eraser)) {
-        $str = preg_replace('/^eraser/', '', $str);
-    } elseif (0 === strpos($str, $erase)) {
-        $str = preg_replace('/^erase/', '', $str);
-    }
-
-    if ($str == '') {
-        return 'YES';
-    } elseif (
-        0 === strpos($str, $dreamer) ||
-        0 === strpos($str, $dream) ||
-        0 === strpos($str, $eraser) ||
-        0 === strpos($str, $erase)
-    ) {
-        return checkString($str);
-    } else {
-        return 'NO';
-    }
-}
-
-echo checkString(trim(fgets(STDIN)));
+  
+echo preg_match('/^(dreamerase|dreamer|eraser|dream|erase){1,}$/', trim(fgets(STDIN))) ? 'YES' : 'NO';
